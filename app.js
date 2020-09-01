0582bb2ea9bebf0cb0e0736bdb09d73b26ada73c
isaac2ngeno5 <isaac2ngeno5@gmail.com>
2020-09-01 15:44:49 +0300

    secenod commit

---
+++ app.js
@@ -0,0 +1,105 @@
+(function(global) {
+
+    // Compatibility
+    navigator.getUserMedia_ = (navigator.getUserMedia || navigator.webkitGetUserMedia
+     || navigator.mozGetUserMedia || navigator.msGetUserMedia);
+  
+    var peerClient;
+    var currentPeerConnection;
+    var localMediaStream;
+  
+    $(function() {
+  
+      var $myselfId = $('#js-myself-id');
+      var $peerId = $('#js-peer-id');
+      var $partnerId = $('#js-partner-id');
+      var $open = $('#js-open');
+      var $connect = $('#js-connect');
+      var videoMyself = document.querySelector('#js-video-myself');
+      var videoPartner = document.querySelector('#js-video-partner');
+  
+      navigator.getUserMedia_({video: true, audio: true}, function(stream) {
+        videoMyself.srcObject = stream;
+        videoMyself.play();
+        localMediaStream = stream;
+      });
+      
+      $open.on('click', function(e) {
+        // create peer object
+        var myselfId = $myselfId.val();
+        peerClient = new Peer();
+  
+        // if peer connection is opened
+        peerClient.on('open', function() {
+          alert(peerClient.id)
+          $peerId.html(peerClient.id);
+        });
+        
+        peerClient.on('call', function(call) {
+          // answer with my media stream
+          console.log('Receiving call', call)
+          call.answer(localMediaStream);
+          
+  
+          // close current connection if exists
+          if (currentPeerConnection) {
+            currentPeerConnection.close();
+          }
+          
+          // keep call as currentPeerConnection
+          currentPeerConnection = call;
+          
+          // wait for partner's stream
+          call.on('stream', function(stream) {
+            videoPartner.src = URL.createObjectURL(stream);
+            videoPartner.play();
+          });
+          
+          // if connection is closed
+          call.on('close', function() {
+            console.log('Connection is closed.');
+          });
+        });
+        
+        // disable id input
+        $myselfId.attr('disabled', 'disabled');
+        
+        // enable partner id input
+        $partnerId.removeAttr('disabled');
+        
+        // enable connect button
+        $connect.removeAttr('disabled');
+      });
+  
+      $connect.on('click', function(e) {
+        // if peerClient is not initialized
+        if (!peerClient) {
+          return;
+        }
+        
+        // connect to partner
+        var partnerId = $partnerId.val();
+        var call = peerClient.call(partnerId, localMediaStream);
+  
+        // close current connection if exists
+        if (currentPeerConnection) {
+          currentPeerConnection.close();
+        }
+  
+        // keep call as currentPeerConnection
+        currentPeerConnection = call;
+  
+        // wait for partner's stream
+        call.on('stream', function(stream) {
+          videoPartner.src = URL.createObjectURL(stream);
+          videoPartner.play();
+        });
+  
+        // if connection is closed
+        call.on('close', function() {
+          console.log('Connection is closed.');
+        });
+      });
+    });
+  
+  })(this);
