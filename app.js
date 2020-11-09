aaf7ce2cc38ab2ee897652c9f935790001d98157
isaac2ngeno5 <isaac2ngeno5@gmail.com>
2020-11-09 10:48:19 +0300

    inital redesign commmit

--- app.js
+++ app.js
@@ -1,103 +1,35 @@
-(function(global) {
+(function(){
+    "use strict";
 
-  // Compatibility
-  navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia;
+    console.log("app loaded");
 
-  var peerClient;
-  var currentPeerConnection;
-  var localMediaStream;
+    const username = document.querySelector("#username");
+    const register = document.querySelector("#register");
+    const form = document.querySelector("#registration");
 
-  $(function() {
+    var peer;
 
-    var $myselfId = $('#js-myself-id');
-    var $peerId = $('#js-peer-id');
-    var $partnerId = $('#js-partner-id');
-    var $open = $('#js-open');
-    var $connect = $('#js-connect');
-    var videoMyself = document.querySelector('#js-video-myself');
-    var videoPartner = document.querySelector('#js-video-partner');
+    const container = document.querySelector(".container");
 
-    navigator.getUserMedia({video: true, audio: true}, function(stream) {
-      videoMyself.src = URL.createObjectURL(stream);
-      videoMyself.play();
-      localMediaStream = stream;
+    form.addEventListener("submit", function(event){
+        event.preventDefault();
+let user = username.value;
+        if(user !== ""){  
+            createPeer(user);          
+        console.log(user);
+        }else{
+            alert("please provide a username");
+        } 
     });
-    
-    $open.on('click', function(e) {
-      // create peer object
-      var myselfId = $myselfId.val();
-      peerClient = new Peer();
 
 
-      // if peer connection is opened
-      peerClient.on('open', function() {
-        alert(peerClient.id)
-        $peerId.html(peerClient.id);
-      });
-      
-      peerClient.on('call', function(call) {
-        // answer with my media stream
-        call.answer(localMediaStream);
-        
-        // close current connection if exists
-        if (currentPeerConnection) {
-          currentPeerConnection.close();
-        }
-        
-        // keep call as currentPeerConnection
-        currentPeerConnection = call;
-        
-        // wait for partner's stream
-        call.on('stream', function(stream) {
-          videoPartner.src = URL.createObjectURL(stream);
-          videoPartner.play();
+    function createPeer(username){
+        peer = new peer(username);
+
+        peer.on("open", function(peerid){
+            console.log(`peer id : ${peerid}`);
         });
-        
-        // if connection is closed
-        call.on('close', function() {
-          console.log('Connection is closed.');
-        });
-      });
-      
-      // disable id input
-      $myselfId.attr('disabled', 'disabled');
-      
-      // enable partner id input
-      $partnerId.removeAttr('disabled');
-      
-      // enable connect button
-      $connect.removeAttr('disabled');
-    });
+    }
 
-    $connect.on('click', function(e) {
-      // if peerClient is not initialized
-      if (!peerClient) {
-        return;
-      }
-      
-      // connect to partner
-      var partnerId = $partnerId.val();
-      var call = peerClient.call(partnerId, localMediaStream);
 
-      // close current connection if exists
-      if (currentPeerConnection) {
-        currentPeerConnection.close();
-      }
-
-      // keep call as currentPeerConnection
-      currentPeerConnection = call;
-
-      // wait for partner's stream
-      call.on('stream', function(stream) {
-        videoPartner.src = URL.createObjectURL(stream);
-        videoPartner.play();
-      });
-
-      // if connection is closed
-      call.on('close', function() {
-        console.log('Connection is closed.');
-      });
-    });
-  });
-
-})(this);
+}(document));
